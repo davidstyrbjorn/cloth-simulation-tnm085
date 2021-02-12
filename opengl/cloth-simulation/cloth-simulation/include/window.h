26e3fb798281f7430624e88de059866e8b920c2c
davst166 <davst166@student.liu.se>
2021-02-12 11:26:22 +0100

    Se window.h för exempel av hur window och input klassen ska funka

--- opengl/cloth-simulation/cloth-simulation/include/window.h
+++ opengl/cloth-simulation/cloth-simulation/include/window.h
@@ -7,6 +7,41 @@
 #include"glm/vec2.hpp"
 #include"glm/vec4.hpp"
 
+/*
+Example use case by user:
+
+#include"window.h"
+#include"input.h"
+
+Window window(400,300);
+Input input(window);
+
+while(window.IsOpen()){
+	
+	for(InputEvent event : input.GetPolledEvents()){
+		// Examples
+		if(event.type == EventType::WINDOW_RESIZE) {} ...
+		if(event.type == EventType::KEY_PRESSED) {} ...
+		if(event.type == EventType::MOUSE_DOWN) {} ...
+		// event.key, event.size.x , event.size.y ...
+	}
+
+	window.Clear(...);
+
+	// Rendering här
+
+	input.CollectEvents();
+	window.Display();
+}
+
+########################################################################################
+
+Window constructor ska skapa GLFW fönstret inget mer
+Input constructor tar in fönstret och sätter alla 'callbacks'
+SE: https://www.glfw.org/docs/3.3.1/input_guide.html#events
+
+*/
+
 class Window {	
 public:
 	Window();

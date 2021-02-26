0dcc71293da051bb4356f54d7cecf81138cd2022
David Styrbjörn <davst166@student.liu.se>
2021-02-26 10:34:30 +0100

    fix

--- opengl/cloth-simulation/cloth-simulation/include/point.h
+++ opengl/cloth-simulation/cloth-simulation/include/point.h
@@ -18,6 +18,7 @@ struct Point {
 	glm::vec3 position		= { 0,0,0 };
 	glm::vec3 velocity		= { 0,0,0 };
 	glm::vec3 acceleration	= { 0,0,0 };
+	glm::vec3 normal = { 0,0,0 };
 	glm::vec3 force = { 0,0,0 };
 	std::vector<Spring> springs;
 	bool isStatic = false;

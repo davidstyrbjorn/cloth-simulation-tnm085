1c5223dac8341916601461e91c348737d7f052ea
emibe986 <emibe986@student.liu.se>
2021-02-26 10:21:43 +0100

    Verlet Integration

--- opengl/cloth-simulation/cloth-simulation/src/cloth.cpp
+++ opengl/cloth-simulation/cloth-simulation/src/cloth.cpp
@@ -79,6 +79,8 @@ void Cloth::CreateGridPoints()
 
 		Point& curr = gridPoints[i];
 
+
+		//if (y_counter == 0 && (x_counter == 0) || y_counter == 0 && x_counter == 15) curr.isStatic = true;
 		if (y_counter == 0 && (x_counter == 0)) curr.isStatic = true;
 		//if (y_counter == 1 && (x_counter == 0 || x_counter == squares_per_side || x_counter == 1 || x_counter == squares_per_side - 1)) curr.isStatic = true;
 		//if (y_counter == 2 && (x_counter == 0 || x_counter == squares_per_side || x_counter == 1 || x_counter == squares_per_side - 1)) curr.isStatic = true;
--- opengl/cloth-simulation/cloth-simulation/src/point.cpp
+++ opengl/cloth-simulation/cloth-simulation/src/point.cpp
@@ -9,8 +9,22 @@ void Step(Point& p, const ClothConfig& clothConfig, float dt)
 {
 	if (p.isStatic) return;
 
-	p.position = p.position + p.velocity * dt; 
-	p.velocity = p.velocity + (calculateForce(p, clothConfig) / clothConfig.mass) * dt; 
+	/*-----Euler-----*/
+	//p.position = p.position + p.velocity * dt; 
+	//p.velocity = p.velocity + (calculateForce(p, clothConfig) / clothConfig.mass) * dt;
+
+	/*-----Verlet----*/
+	
+	// newAcc is used to save the new acceleration and allow both the new and old to be used during calculations
+	glm::vec3 newAcc = calculateForce(p, clothConfig) / clothConfig.mass;
+
+	//Calculate our new position and velocity
+	p.position = p.position + p.velocity * dt + p.acceleration* (dt * dt * 0.5f);
+	p.velocity = p.velocity + (p.acceleration + newAcc) * (dt * 0.5f);
+	
+	//Set newAcc to our current acceleration
+	p.acceleration = newAcc;
+
 }
 
 glm::vec3 calculateForce(Point& p, const ClothConfig& clothConfig)
